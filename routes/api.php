2261ea5bf2e5d82f6a3f7c85b26b3728ebd2aaba
Yidir <yader.dev@gmail.com>
2019-07-03 19:34:29 +0100

    Switch cors handling to barryvdh/laravel-cors
    Turns out there's more logic with cors when dealing with PUT & DELETE
    methods, not worth to implement for us right now, just use the package.
    
    Signed-off-by: Yidir <yader.dev@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,8 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(['middleware' => ['cors']], function () {
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-    Route::resource('keys', 'ProductKeyController');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
+Route::resource('keys', 'ProductKeyController');
