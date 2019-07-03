71de3a57fdd54ab68add4c67f0e23a61a62b9585
Yidir <yader.dev@gmail.com>
2019-07-04 00:18:19 +0100

    Add Middleware CORS fix
    A less-dirty fix for the CORS problem, works on local env (others did,
    too).
    
    Signed-off-by: Yidir <yader.dev@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,3 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::resource('keys', 'ProductKeyController');
-
-header('Access-Control-Allow-Origin:  *');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
